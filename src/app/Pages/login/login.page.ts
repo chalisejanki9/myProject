94d8ba6dfa7890f6b7ed9036e8857e1ae0908b28
janki <50244392+chalisejanki9@users.noreply.github.com>
2020-05-11 19:52:55 -0400

    i First commit :wq

---
+++ src/app/Pages/login/login.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  LoginForm: FormGroup;
+
+  error_messages={
+    'userName':[
+      {type: 'required', message:'UserName is required'},
+      {type: 'minLength', message:'minLength is 5'}
+    ],
+    'password':[
+      {type: 'required', message:'Password is required'}
+     
+    ],
+  }
+
+  constructor(
+    public formBuilder: FormBuilder) { 
+    this.LoginForm = this.formBuilder.group({
+      userName : new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ])),
+      password :new FormControl('',Validators.compose([
+        Validators.required
+       
+      ]))
+    });
+
+  }
+
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-outline';
+  
+ 
+  hideShowPassword() {
+     this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+     this.passwordIcon = this.passwordIcon === 'eye-outline' ? 'eye-off-outline' : 'eye-outline';
+  }
+
+
+
+  ngOnInit() {
+  }
+
+}
